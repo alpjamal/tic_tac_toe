5c38d7d480a75708b4955b05579746f44e3b49b8
alp_jamal <mr.abdujamol1@mail.ru>
2022-11-24 16:38:03 +0500

    create homepage.dart file and modify main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TictacToeHomePage(),
-      routes: {},
+      routes: {
+        '/': (context) => TictacToeHomePage(),
+      },
     );
   }
 }
