a0a601808ffb8ab6706b56af37bbb71ce95a4427
alp_jamal <mr.abdujamol1@mail.ru>
2022-11-24 16:30:51 +0500

    Create new project named tic tac toe

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TictacToeHomePage(),
+      routes: {},
+    );
+  }
+}
+
+class TictacToeHomePage extends StatelessWidget {
+  TictacToeHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomePage'),
+      ),
+    );
+  }
+}
